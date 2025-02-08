59dcd4260fdac68e22c0098776ef0303944a79f3
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-02-08 21:56:41 +0530

    icon , top frame add

--- main.py
+++ main.py
@@ -7,7 +7,19 @@ import os
 
 root = Tk()
 root.title("Text to Speech")
-root.geometry("900x450")
+root.geometry("900x450+200+200")
 root.resizable(False, False)
+root.configure(bg="#305065")
+
+#icon
+image_icon =PhotoImage(file="speak.png") 
+root.iconphoto(False, image_icon)
+
+#top frame
+Top_frame = Frame(root, bg="white", width=900, height=100)
+Top_frame.place(x=0, y=0)
+
+Logo = PhotoImage(file="speaker logo.png")
+Label(Top_frame, image = Logo, bg="white").place(x=15, y=5)
 
 root.mainloop()
